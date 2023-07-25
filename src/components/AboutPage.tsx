c2a023cfd4581bf75f3760f5676c057e87c6083f
Gui2912 <dainize22@gmail.com>
2023-07-25 14:30:08 -0300

    feat: AboutPage

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -1,4 +1,4 @@
-import { Section } from "@/components";
+import { Button, Section } from "@/components";
 import React from "react";
 
 interface AboutInfos {
@@ -34,35 +34,43 @@ const aboutInformations: AboutInfos[] = [
     },
     {
         label: "Phone",
-        information: "+5519992530418"
-    }
+        information: "+5519992530418",
+    },
 ];
 
 const AboutPage = () => {
     return (
-
-            <Section className="">
-                <h2 className="text-start w-full font-bold text-5xl">
-                    About-me
-                </h2>
-                <p className="text-xl text-gray-400 my-5">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Reiciendis saepe laborum deleniti distinctio ea officia
-                    incidunt nulla blanditiis, dolorum reprehenderit libero esse
-                    dolores autem, porro nisi excepturi ut. Odio, minima!
+        <Section className="" id='about'>
+            <h2 className="text-start w-full font-bold text-5xl">About-me</h2>
+            <p className="text-xl text-gray-400 my-5">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Reiciendis saepe laborum deleniti distinctio ea officia incidunt
+                nulla blanditiis, dolorum reprehenderit libero esse dolores
+                autem, porro nisi excepturi ut. Odio, minima!
+            </p>
+            <article className=" w-full">
+                <ul className="flex flex-col gap-4 items-start">
+                    {aboutInformations.map((elt, index) => (
+                        <li className="flex gap-3 items-center" key={index}>
+                            <h3 className="font-bold text-xl min-w-[90px]">
+                                {elt.label}:{" "}
+                            </h3>
+                            <p className="text-gray-400 text-xl">
+                                {elt.information}
+                            </p>
+                        </li>
+                    ))}
+                </ul>
+                <p className="text-xl mt-12 mb-6">
+                    <span className="text-primary font-bold text-xl">120</span>{" "}
+                    Projects completed
                 </p>
-                <article>
-                    <ul>
-                        {aboutInformations.map((elt, index) => (
-                            <li className='flex gap-3' key={index}>
-                                <h3 className='font-bold'>{elt.label}</h3>
-                                <p>{elt.information}</p>
-                            </li>
-                        ))}
-                    </ul>
-                </article>
-            </Section>
-
+                <Button className='btn-rounded'>Dowload Resume</Button>
+            </article>
+            <ul>
+                
+            </ul>
+        </Section>
     );
 };
 
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -3,12 +3,14 @@ import React from "react";
 interface SectionProps {
     children: React.ReactNode;
     className: string;
+    id?:string;
 }
 
-const Section = ({ className, children }: SectionProps) => {
+const Section = ({ className, children, id }: SectionProps) => {
     return (
         <section
-            className={`${className} pt-12 min-h-screen w-full flex max-w-5xl z-20 items-center flex-col`}
+            id={id}
+            className={`${className} pt-12 px-4 min-h-screen w-full flex max-w-5xl z-20 items-center flex-col`}
         >
             {children}
         </section>
